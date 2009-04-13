9df7a74b1f8986ef4d6f6fb50185b7332c7e15f8
Schuyler Erle <sderle@goldman.(none)>
2009-04-13 00:25:31 -0400

    Add webui copied from RapidSMS distro.

---
+++ apps/webui/views.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4
+
+
+from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+
+def dashboard(req):
+    return render_to_response("dashboard.html", { },
+        context_instance=RequestContext(req))
