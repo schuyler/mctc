1889ad6ed7facadf47944453c43eb5ac88c9ddb1
Andy McKay <andy@Macintosh-2.local>
2009-04-19 18:26:24 +0300

    Move observations to a model and add in the malaria part.

---
+++ utils/dump.py
@@ -0,0 +1,10 @@
+# note this requires the settings hack
+import sys
+import os
+path = os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(__file__))))
+sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+
+os.system('python "%s/django-manage.py" dumpdata mctc.observation --indent=2 > "%s/apps/mctc/fixtures/observations.json"' % (path, path))
+os.system('python "%s/django-manage.py" dumpdata mctc.diagnosis --indent=2 > "%s/apps/mctc/fixtures/diagnoses.json"' % (path, path))
